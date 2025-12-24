33faf5526f41b4dae0b1d8d57dfcfe14b9c25adb
Nehad <nehad.elsayed@robustastudio.com>
2025-12-25 01:31:34 +0200

    resolve navbar error

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,16 +1,11 @@
-import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 
 export default function Layout() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const handleLogout = () => {
-    setIsLoggedIn(false);
-  };
   return (
     <div className="min-h-screen ">
-      <Navbar isLoggedIn={isLoggedIn} onLogout={handleLogout} />
+      <Navbar  />
 
       <Outlet />
     </div>
